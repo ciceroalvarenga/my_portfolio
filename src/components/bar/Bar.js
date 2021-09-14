10af8f0fd9d63ddbd135af0cffc767c8c6d87bc0
Cícero Alvarenga <cicero.alvarenga@orgsystem.com.br>
2021-09-14 17:32:50 -0300

    Animação das barras de progresso

--- src/components/bar/Bar.js
+++ src/components/bar/Bar.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './bar.css';
 
 export const Bar = ({ value: { icon, name, level } }) => {
-  const bar_width = `${level}%`;
+  const [style, setStyle] = useState({});
+  const [translucit, setTranslucit] = useState({});
+
+  setTimeout(() => {
+    const newStyle = {
+      opacity: 1,
+      width: `${level}%`,
+    };
+    setStyle(newStyle);
+  });
 
   return (
-    <div className="bar">
-      <div className="bar_wrapper" style={{ width: bar_width }}>
-        <span className="bar_name">
-          <img src={icon} alt="icon" className="bar_icon mr-2" />
-          {name}
-        </span>
+    <>
+      <div className="bar">
+        <div className="bar_wrapper" style={style}>
+          <span className="bar_name">
+            <img src={icon} alt="icon" className="bar_icon mr-2" />
+            {name}
+          </span>
+        </div>
+        <div className="spnier" />
       </div>
-    </div>
+    </>
   );
 };
--- src/components/project/Projects.js
+++ src/components/project/Projects.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data_projects from '../../data/projects_data';
+import { ProjectCard } from '../projectCard/ProjectCard';
+
+import './projects.css';
 
 export const Projects = () => {
+  const [projects, setProjects] = useState(data_projects);
+
+  const handleFilterCategory = (name) => {
+    console.log('clico');
+    const new_array = data_projects.filter((project) =>
+      project.category.includes(name)
+    );
+    setProjects(new_array);
+  };
+
   return (
-    <div>
-      <h1>Projectsssssssssssss</h1>
+    <div className="container projects">
+      <div className="projects_navbar">
+        <div onClick={() => setProjects(data_projects)}>all</div>
+        <div onClick={() => handleFilterCategory('react')}>React</div>
+        <div onClick={() => handleFilterCategory('mongoDB')}>MongoDb</div>
+        <div onClick={() => handleFilterCategory('node.js')}>Node</div>
+        <div onClick={() => handleFilterCategory('vanilla')}>Vanilla</div>
+      </div>
+      <div className="row">
+        {projects.map((project) => (
+          <ProjectCard key={project.name} project={project} />
+        ))}
+      </div>
     </div>
   );
 };
